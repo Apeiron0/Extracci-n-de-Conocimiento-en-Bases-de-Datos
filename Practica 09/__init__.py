6b1d07fc5a7f40d79d01aff284f84a87e4740396
Victor Martinez <studioquetzal@gmail.com>
2026-06-08 20:05:50 -0600

    Agragada practica 09

---
+++ Practica 09/__init__.py
@@ -0,0 +1,158 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# # Datos base para los ejemplos
+x = np.linspace(0, 10, 25)
+y = np.sin(x) + x / 2
+
+# Gráfico de líneas básico
+fig, ax = plt.subplots()
+ax.plot(x, y)
+plt.show()
+
+# ######################################
+# ## COLOR DE LINEA
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Datos
+x = np.linspace(0, 10, 25)
+y = np.sin(x) + x / 2
+
+# Gráfico de líneas
+fig, ax = plt.subplots()
+ax.plot(x, y, color="r")
+plt.show()
+
+# ######################################
+# ### GROSOR DE LINEA
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Datos
+x = np.linspace(0, 10, 25)
+y = np.sin(x) + x / 2
+
+# Gráfico de líneas
+fig, ax = plt.subplots()
+ax.plot(x, y, linewidth=4)
+# ax.plot(x, y, lw = 4) # Equivalente
+plt.show()
+
+# ######################################
+# ### ESTILO DE LINEA
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Datos
+x = np.linspace(0, 10, 25)
+y = np.sin(x) + x / 2
+
+# Gráfico de líneas
+fig, ax = plt.subplots()
+ax.plot(x, y, linestyle="--")
+# ax.plot(x, y, ls = "--") # Equivalente
+plt.show()
+
+# ######################################
+# ### AGREGAR SIMBOLOS A LAS LINEAS
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Datos
+x = np.linspace(0, 10, 25)
+y = np.sin(x) + x / 2
+
+# Gráfico de líneas
+fig, ax = plt.subplots()
+ax.plot(x, y, marker="o", markersize=5)
+# ax.plot(x, y, marker = "o", ms = 5) # Equivalente
+plt.show()
+
+# ######################################
+# ## LINEAS Y SIMBOLOS CON COLORES
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Datos
+x = np.linspace(0, 10, 25)
+y = np.sin(x) + x / 2
+
+# Gráfico de líneas
+fig, ax = plt.subplots()
+ax.plot(x, y, color="r", marker='s', markerfacecolor='black', markeredgecolor='black')
+# ax.plot(x, y, color = "r", marker = 's', mfc = 'black', mec = 'black') # Equivalente
+plt.show()
+
+# ######################################
+# ### USAR FORMATO
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Datos
+x = np.linspace(0, 10, 25)
+y = np.sin(x) + x / 2
+
+# Gráfico de líneas
+fig, ax = plt.subplots()
+ax.plot(x, y, "*g-")
+plt.show()
+
+# ######################################
+# #### GRAFICA CON VARIAS LINEAS
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Datos
+x1 = np.linspace(0, 10, 25)
+y1 = np.sin(x1) + x1 / 2
+
+x2 = np.linspace(0, 10, 25)
+y2 = np.cos(x2) + x2 / 2
+
+# Gráfico de líneas
+fig, ax = plt.subplots()
+ax.plot(x1, y1, marker="o", label="Sin(x) + x/2")
+ax.plot(x2, y2, marker="o", label="Cos(x) + x/2")
+ax.legend()
+plt.show()
+
+# ######################################
+# ######## GRAFICA DE VARIOS NIVELES
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Datos
+x1 = np.linspace(0, 10, 25)
+y1 = np.sin(x1) + x1 / 2
+
+x2 = np.linspace(0, 10, 25)
+y2 = np.cos(x2) + x2 / 2
+
+# Gráfico de líneas
+fig, ax = plt.subplots()
+ax.plot(x1, y1, "c^", x2, y2, "d--")
+plt.show()
+
+# ######################################
+# ##### FUNCION STEP
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Datos
+x = np.linspace(0, 10, 25)
+y = np.sin(x) + x / 2
+
+# Gráfico de líneas
+fig, ax = plt.subplots()
+ax.step(x, y)
+plt.show()
